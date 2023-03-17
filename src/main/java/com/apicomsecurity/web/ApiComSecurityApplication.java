9cf0ad9e847b308eeabbce5fdf016c6b6d92d1f4
Paulo Gustavo <pauloestudando159@gmail.com>
2023-03-17 09:38:12 -0300

    inicio do projeto

---
+++ src/main/java/com/apicomsecurity/web/ApiComSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.apicomsecurity.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiComSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiComSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apicomsecurity/web/ApiComSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apicomsecurity.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiComSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
