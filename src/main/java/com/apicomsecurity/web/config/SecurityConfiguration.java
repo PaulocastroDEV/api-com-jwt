08ae8c49fbbc7f76b959cc21c436e1b347d32d64
Paulo Gustavo <pauloestudando159@gmail.com>
2023-03-17 11:29:33 -0300

    Configuracao das rotas que sao permitidas a todos e as que terao autenticacao

---
+++ src/main/java/com/apicomsecurity/web/config/SecurityConfiguration.java
@@ -0,0 +1,49 @@
+package com.apicomsecurity.web.config;
+
+import javax.servlet.Filter;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+	@Autowired
+	private JwtAuthenticationFilter jwtAuthFilter;
+	@Autowired
+	private AuthenticationProvider authenticationProvider;
+	
+	
+	
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		
+		http
+			.csrf()
+			.disable()
+			.authorizeHttpRequests()
+			.antMatchers("/login")
+			.permitAll()
+			.anyRequest()
+			.authenticated()
+			.and()
+			.sessionManagement()
+			.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			.and()
+			.authenticationProvider(authenticationProvider)
+			.addFilterBefore(jwtAuthFilter, (Class<? extends Filter>) UsernamePasswordAuthenticationFilter.class);
+			
+			
+		return http.build();
+	}
+}
--- src/main/java/com/apicomsecurity/web/domain/controller/UserController.java
+++ src/main/java/com/apicomsecurity/web/domain/controller/UserController.java
@@ -3,6 +3,7 @@ package com.apicomsecurity.web.domain.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,4 +22,5 @@ public class UserController {
 		return ResponseEntity.status(HttpStatus.CREATED).body(userRepository.save(user));
 		
 	}
+
 }
